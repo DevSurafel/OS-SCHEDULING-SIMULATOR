104befe5826259d71d9370a472f30bd0f14ecf8a
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-02-08 03:56:43 +0530

    Update bot.js

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -1,9 +1,9 @@
 const { Telegraf } = require("telegraf");
 
+const bot = new Telegraf(process.env.REACT_APP_TELEGRAM_BOT_TOKEN);
+
 const web_link = "https://tg-frontend.netlify.app/";
 const community_link = "https://t.me/+p9ThUnIaaV0wYzZk";
-
-const bot = new Telegraf(process.env.REACT_APP_TELEGRAM_BOT_TOKEN);
 
 const welcomeMessage = (user) => {
   const userName = user.username ? `@${user.username}` : user.first_name;
@@ -24,36 +24,39 @@ const createReplyMarkup = (startPayload) => {
   };
 };
 
-// Respond to /start cmd
+// Respond to /start command
 bot.start((ctx) => {
   const startPayload = ctx.startPayload;
   const user = ctx.message.from;
   return ctx.replyWithMarkdown(welcomeMessage(user), { 
-    disable_web_page_preview: true,  // Disable link preview
+    disable_web_page_preview: true,
     ...createReplyMarkup(startPayload) 
   });
 });
 
-// Respond to any message sent to the bot
-bot.on('message', (ctx) => {
-  const startPayload = ctx.startPayload || ''; // You can customize this if needed
+// Respond to any message
+bot.on("message", (ctx) => {
+  const startPayload = ctx.startPayload || "";
   const user = ctx.message.from;
   return ctx.replyWithMarkdown(welcomeMessage(user), { 
-    disable_web_page_preview: true,  // Disable link preview
+    disable_web_page_preview: true,
     ...createReplyMarkup(startPayload) 
   });
 });
 
+// Netlify function handler
 exports.handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
-
-  try {
-    await bot.handleUpdate(JSON.parse(event.body));
-    return { statusCode: 200, body: 'OK' };
-  } catch (e) {
-    console.error('Error in handler:', e);
-    return { statusCode: 400, body: `Bad Request: ${e.message}` };
+  if (event.httpMethod === "POST") {
+    try {
+      await bot.handleUpdate(JSON.parse(event.body));
+      return { statusCode: 200, body: "OK" };
+    } catch (error) {
+      console.error("Error handling update:", error);
+      return { statusCode: 400, body: `Bad Request: ${error.message}` };
+    }
+  } else if (event.httpMethod === "GET") {
+    return { statusCode: 200, body: "Bot is running!" };
+  } else {
+    return { statusCode: 405, body: "Method Not Allowed" };
   }
 };
